7ce16de7de4fbed623b579e2fa41de2089979d47
arvindvepa <arvind.m.vepa@gmail.com>
2019-08-09 00:19:56 -0700

    debug

--- PythonAPI/pycocotools/cocoeval.py
+++ PythonAPI/pycocotools/cocoeval.py
@@ -285,6 +285,8 @@ class COCOeval:
         if p is None:
             p = self.params
         p.catIds = p.catIds if p.useCats == 1 else [-1]
+        print("Category IDs (accumulate): {}".format(p.catIds))
+        print("IOU Thresholds (accumulate): {}".format(p.iouThrs))
         T = len(p.iouThrs)
         R = len(p.recThrs)
         K = len(p.catIds) if p.useCats else 1
@@ -299,6 +301,7 @@ class COCOeval:
         # create dictionary for future indexing
         _pe = self._paramsEval
         catIds = _pe.catIds if _pe.useCats else [-1]
+        print("Category IDs (accumulate and eval): {}".format(_pe.catIds))
         setK = set(catIds)
         setA = set(map(tuple, _pe.areaRng))
         setM = set(_pe.maxDets)
@@ -424,6 +427,7 @@ class COCOeval:
                 # dimension of precision: [TxRxKxAxM]
                 s = self.eval['precision']
                 # IoU
+                print("iouThr: {}, p.iouThrs: {}, bool cond: {}".format(iouThr, p.iouThrs, iouThr == p.iouThrs))
                 if iouThr is not None:
                     t = np.where(iouThr == p.iouThrs)[0]
                     s = s[t]
